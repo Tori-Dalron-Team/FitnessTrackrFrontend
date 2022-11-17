8e7b4a844c7f30f96f859eb0620833ea9d550be1
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-11-17 16:06:22 -0600

    Part 1 Infrastructures

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
 
  const Homepage = () => {
+
+    const
+    // set state
+
+    useEffect(() => {
+        async function  () {
+
+
      return (
          <div>
-             <p>Bonjou!</p>
+            <H2>Workout Buddys</H2>
+             <p>A Home for All to Connect and Exercise!</p>
+             <p>Login to See Today's Top Routines and Activities!</p>
          </div>
      )
  };
 
  export default Homepage;
+
+ // Wants
+    // Quick Description
+    // Login/Register 
+    // Find Better Name
--- src/components/IndexForHomepage.js
+++ src/components/IndexForHomepage.js
@@ -1,9 +1,68 @@
-import React from "react"
+import React, {useState, useEffect } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import Navbar from "./Navbar";
+
+// URL to make template literal
+const apiBaseURL = "https://fitnesstrac-kr.herokuapp.com/api";
 
 const IndexForHomepage = () => {
+    const [activities, setActivities] = useState([]);
+    const [id, setId] = useState("");
+    const [routines, setRoutines] = useState([]);
+    const [username, setUsername] = useState("");
+
+
+    // Fetching Post
+    // Fetch Activities
+    useEffect(() => {
+        async function fetchActivities() {
+            try {
+                const response = await fetch (`${apiBaseURL}/activities`, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                })
+                const data = await response.json();
+
+                setActivities(data)
+            } catch (error) {
+                console.error(error.detail)
+            }
+        }
+        fetchActivities();
+
+    }, []);
+
+    // Fetch Routines
+    useEffect(() => {
+        async function fetchRoutines() {
+            try {
+                const response = await fetch (`${apiBaseURL}/routines`, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                })
+                const data = await response.json();
+
+                setRoutines(data)
+            } catch (error) {
+                console.error
+            }
+        }
+        fetchRoutines();
+
+    }, []);
+
+
+
+
     return (
         <div>
-            <p></p>
+            <div id="nav-container">
+                <h1>Workout Buddys</h1>
+                <Navbar />
+            </div>
+            <Outlet context={[activities, routines, username, id]}/>
         </div>
     )
 };
