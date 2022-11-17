396396fdd4c9badf9434a2e8065a4dcbf3f25b47
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-11-17 16:17:22 -0600

    updates

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,18 +5,12 @@ import { Outlet } from "react-router-dom";
 
  const Homepage = () => {
 
-    const
-    // set state
-
-    useEffect(() => {
-        async function  () {
-
 
      return (
          <div>
-            <H2>Workout Buddys</H2>
-             <p>A Home for All to Connect and Exercise!</p>
-             <p>Login to See Today's Top Routines and Activities!</p>
+            <Navbar />
+
+            <Outlet />
          </div>
      )
  };
