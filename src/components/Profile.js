e92ad4b219e40edcaeb82ab4f190c3d34f917c3a
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-11-20 21:34:58 -0600

    added error image

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,162 +3,74 @@ import { useOutletContext, useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 const Profile = () => {
-    const [personalRoutines, setPersonalRoutines] = useOutletContext();
+    const [personalRoutines, setPersonalRoutines] = useOutletContext()
+    const [allRoutines, setAllRoutines] = useOutletContext()
     const [allActivities, setAllActivities] = useOutletContext()
     const [personalActivities, setPersonalActivities] = useState([]);
     const [routineArray, setRoutineArray] = useState([]);
     const [activitiesArray, setActivitiesArray] = useState([])
     const [activity, setActivity] = useState([]);
-    const [myProfile, setMyProfile] = useOutletContext();
-    const [routine, setRoutine] = useOutletContext()
-    const [everyonesRoutines, setEveryonesRoutines] = useOutletContext();
-    const navigate = useNavigate();
 
-    
-
-
-    useEffect(() => {
-        if (personalRoutines.length) {
-            const newArray = personalRoutines.filter((name) => {
-                
-                return name
-            })
-            setRoutineArray(newArray)
-        }
-    }, [personalRoutines])
     // useEffect(() => {
-    //     if (personalActivities.length) {
-    //         const newActivityArray = personalActivities.filter((name) => {
-                
-    //             return name
+    //     if (personalRoutines) {
+    //         const routArray = personalRoutines.filter((name) => {
+    //             console.log("this is name", personalRoutines)
+    //             return name.goal
     //         })
-    //         setActivitiesArray(newActivityArray)
+    //         setRoutineArray(routArray)
+    //     }
+    // }, [personalRoutines])
+    // useEffect(() => {
+    //     if (personalActivities) {
+    //         const actArray = personalActivities.filter((name) => {
+    //             return name.description 
+    //         })
+    //         setActivitiesArray(actArray)
     //     }
     // }, [personalActivities])
-    useEffect(() => {
-        if(localStorage.getItem("token")) {
-            async function profileInfo() {
-                try {
-                    const response = await fetch ('https://fitnesstrac-kr.herokuapp.com/api/users/me', {
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Authorization': `Bearer ${localStorage.getItem("token")}`
-                        }
-                    })
-                    const data = await response.json()
-                    setMyProfile(data)
-                } catch (error) {
-                    console.log(error)
-                }
-            }
-            profileInfo();
-        }
-        
-    }, [])
-    useEffect(() => {
-        async function myRoutines() {
-            try {
-                const response = await fetch(`https://fitnesstrac-kr.herokuapp.com/api/users/${myProfile.username}/routines`, {
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                })
-                const data = await response.json();
-                setPersonalRoutines(data)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        if(myProfile.username) {myRoutines()}
-    }, [myProfile]);
 
-        async function deleteButton(id) {
+    useEffect(() => {
+        async function profileInfo() {
+            
             try {
-                const response = await fetch (`http://fitnesstrac-kr.herokuapp.com/api/routines/${id}`, {
-                    method: "DELETE",
+                const response = await fetch("http://fitnesstrac-kr.herokuapp.com/api/users/me", {
                     headers: {
                         'Content-Type': 'application/json',
                         'Authorization': `Bearer ${localStorage.getItem("token")}`
                     }
                 })
                 const data = await response.json()
-                console.log(data)
-                setEveryonesRoutines(everyonesRoutines.filter((routine) => {
-                    return routine.id != id
-                }))
-                setPersonalRoutines(personalRoutines.filter((routine) => {
-                    return routine.id != id
-                }))
-                navigate("/profile")
-                
+                setPersonalRoutines(data.name)
+                setPersonalActivities(data.name)
             } catch (error) {
                 console.log(error)
             }
         }
-    // useEffect(() => {
-    //     async function myActivities() {
-    //         try {
-    //             const response = await fetch (`https://fitnesstrac-kr.herokuapp.com/api/activities/${myProfile.username}/routines`, {
-    //                 headers: {
-    //                     'Content-Type': 'application/json'
-    //                 }
-    //             })
-    //             const data = await response.json();
-    //             console.log(data)
-    //             setPersonalActivities(data)
-    //         } catch (error) {
-    //             console.log(error)
-    //         }
-    //     }
-    //     if(myProfile.username) {myActivities()}
-    // }, [myProfile])
-
-    function logOut(event) {
-        localStorage.removeItem("token")
-        navigate("/")
-    }
+        profileInfo()
+    }, [])
     return (
         <div>
-
             <h3 id="profile">My Profile</h3>
             <div id="profile-items">
-
-            
                 {
                     routineArray.length ? routineArray.map((routine, idx) =>{
                         return <div key={idx}>
-                            <h4>{routine.name}</h4>
-                            <p>{routine.goal}</p>
-                            <button onClick={(e) => {
-                                e.preventDefault()
-                                deleteButton(routine.id)}}>Delete</button>
-                            <Link to={`/editroutine/${routine.rotuineId}`}>Edit Routine</Link>
+                            <p>Your Routines: {goal}</p>
                         </div>
-                        
                     }) : <p>There are no Routines to view</p>
                 }
                 </div>
-
-
-                <div id="profile-items"></div>
-
-                
-
-            
-
-                {/* {
+                <div id="profile-items">
+                {
                     activitiesArray.length ? activitiesArray.map((activity, idx) => {
                         return <div key={idx}>
-                            <p>{activity.name}</p>
-                            <Link to={`/editactivities/${activity.activityId}`}>Edit Activity</Link>
+                            <p>Your Activities: {duration}</p>
                         </div>
                     }) : <p>There are no Activities to view</p>
-                } */}
-
-            
+                }
+            </div>
             <div id="profile-items"><Link to="/createroutine">Create New Routine</Link></div>
             <div id="profile-items"><Link to="/createactivities">Create New Activity</Link></div>
-                <button onClick={logOut}>Log Out</button>
         </div>
     )
 };
